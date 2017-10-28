be8a471e89296a11f0370e657ca8a4daa3a51d83
Jordy Jackson Antunes da Rocha <jordy.antunes@hotmail.com>
2017-10-28 17:38:00 -0200

    Scripts atualizados para uma interface comum

--- src/frameDiff.py
+++ src/frameDiff.py
@@ -9,23 +9,26 @@ def frameDiff(video):
 
 	while(video.isOpened()):
 		rec_2, frame_2 = video.read()
-		if rec_2 ==True:
+		if rec_2 == True:
 			y = np.sum(frame_1)
 			dY = np.sum(np.absolute(np.subtract(frame_1,frame_2)))
 			rec_1, frame_1 = video.read()
 			descritores = np.append(descritores, np.array([[dY, y]]), axis = 0)
-			print(descritores)
+			# print(descritores)
 		else:
 			break
 	
 	return descritores 
 
-def main():
-	cap = cv2.VideoCapture(sys.argv[1])
+def run(video_path, filename):
+	cap = cv2.VideoCapture(video_path)
 	resultados = frameDiff(cap)
-	print(resultados[0])
-
+	np.savetxt("{}_framediff.csv".format(filename), resultados, delimiter=",")
 	cap.release()
+
+	
+def main():
+	run(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__":
 	main()
