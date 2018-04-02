b17d5d2e22139dacb27262d886bbaa1ab7ee7665
Jordy Jackson Antunes da Rocha <jordy.antunes@hotmail.com>
2018-04-01 22:42:12 -0300

    banco de dados criado

---
+++ db/init.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS algoritmos (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	nome CHAR(50));
+
+CREATE TABLE IF NOT EXISTS comparadores (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	nome CHAR(50));
+
+CREATE TABLE IF NOT EXISTS distorcoes (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	descricao CHAR(50));
+
+CREATE TABLE IF NOT EXISTS videos (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	filename CHAR(200),
+	distorcao_id INTEGER NULL,
+	video_original INTEGER NULL, 
+	FOREIGN KEY(distorcao_id) REFERENCES distorcoes(id),
+	FOREIGN KEY(video_original	) REFERENCES video(id));
+
+CREATE TABLE IF NOT EXISTS assinaturas (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id_video INTEGER NOT NULL,
+	id_algoritmo INTEGER,
+	FOREIGN	KEY(id_video) REFERENCES videos(id),
+	FOREIGN	KEY(id_algoritmo) REFERENCES algoritmos(id));
+
+CREATE TABLE IF NOT EXISTS testes (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	id_video_original INTEGER,
+	id_video_teste INTEGER,
+	id_comparador INTEGER,
+	data_criacao DATETIME,
+	resultado_bruto REAL,
+	resultado_copia BOOLEAN,
+	FOREIGN	KEY(id_video_original) REFERENCES videos(id),
+	FOREIGN	KEY(id_video_teste) REFERENCES videos(id),
+	FOREIGN	KEY(id_comparador) REFERENCES comparadores(id));
